13c5c572d53a45ee9ffdfb2b102082f98be67212
Robert Krimen <rkrimen@cpan.org>
2007-10-13 22:51:29 +0000

    Import of RKRIMEN/Hash-Dirty-0.023 from CPAN.
    gitpan-cpan-distribution: Hash-Dirty
    gitpan-cpan-version:      0.023
    gitpan-cpan-path:         RKRIMEN/Hash-Dirty-0.023.tar.gz
    gitpan-cpan-author:       RKRIMEN
    gitpan-cpan-maturity:     released

--- lib/Hash/Dirty.pm
+++ lib/Hash/Dirty.pm
@@ -9,11 +9,11 @@ Hash::Dirty - Keep track of whether a hash is dirty or not
 
 =head1 VERSION
 
-Version 0.022
+Version 0.023
 
 =cut
 
-our $VERSION = '0.022';
+our $VERSION = '0.023';
 
 =head1 SYNOPSIS
 
